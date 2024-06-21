bb8298a8d03a73af3220e7d5961edc46337cb66e
astha@ekak.in <astha123@#>
2024-06-21 10:51:14 +0530

    updated index.html file

--- app.py
+++ app.py
@@ -7,6 +7,12 @@ def homepage():
     return render_template("main.html")
 
 
+@app.route('/dashboard/')
+def dashboard():
+    #return render_template("main.html")
+    return "hy this is my dashboard page"
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
